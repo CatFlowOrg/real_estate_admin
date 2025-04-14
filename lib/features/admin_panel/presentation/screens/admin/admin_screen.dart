d1b5c5a83fbc7352cd1abe2066f2d4e22937b870
Nikola Rankovic <nikolarankovic2810@gmail.com>
2025-04-14 10:33:06 +0200

    del form repo

--- lib/features/admin_panel/presentation/screens/admin/admin_screen.dart
+++ lib/features/admin_panel/presentation/screens/admin/admin_screen.dart
@@ -18,8 +18,8 @@ class AdminScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider(create: (context) => getIt<AgentBloc>()..add(const LoadAgentsEvent())),
-        BlocProvider(create: (context) => getIt<AuthBloc>()..add(const GetUserInfo())),
+        BlocProvider(create: (_) => getIt<AgentBloc>()..add(const LoadAgentsEvent())),
+        BlocProvider(create: (_) => getIt<AuthBloc>()..add(const GetUserInfo())),
       ],
       child: const _AdminScreenContent(),
     );
@@ -44,33 +44,6 @@ class _AdminScreenContentState extends State<_AdminScreenContent> {
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              BlocBuilder<AuthBloc, AuthState>(
-                builder: (context, state) {
-                  if (state is GetUserInfoSuccess) {
-                    return HeaderSectionAdminPanel(userResponse: state.userResponse);
-                  } else if (state is AuthStateInitial) {
-                    return const Center(child: CircularProgressIndicator());
-                  } else {
-                    return const SizedBox.shrink();
-                  }
-                },
-              ),
-              const SizedBox(height: 24),
-              const TotalRevenueCard(),
-              const SizedBox(height: 16),
-              BlocBuilder<AgentBloc, AgentState>(
-                builder: (context, state) {
-                  Widget agentCard;
   void toggleRevenueCardExpansion() {
     setState(() {
       isRevenueCardExpanded = !isRevenueCardExpanded;
@@ -79,14 +52,16 @@ class _AdminScreenContentState extends State<_AdminScreenContent> {
   }
 
   Widget _buildHeader() {
-    return const Padding(
-      padding: EdgeInsets.only(bottom: 24),
-      child: HeaderSectionAdminPanel(
-        userName: "Djordje Tovilovic",
-        userRole: "Admin",
-        imageUrl:
-            "https://media2.dev.to/dynamic/image/width=800%2Cheight=%2Cfit=scale-down%2Cgravity=auto/https%3A%2F%2Fwww.gravatar.com%2Favatar%2F2c7d99fe281ecd3bcd65ab915bac6dd5%3Fs%3D250",
-      ),
+    return BlocBuilder<AuthBloc, AuthState>(
+      builder: (context, state) {
+        if (state is GetUserInfoSuccess) {
+          return HeaderSectionAdminPanel(userResponse: state.userResponse);
+        } else if (state is AuthStateInitial) {
+          return const Center(child: CircularProgressIndicator());
+        } else {
+          return const SizedBox.shrink();
+        }
+      },
     );
   }
 
@@ -127,40 +102,45 @@ class _AdminScreenContentState extends State<_AdminScreenContent> {
           padding: const EdgeInsets.all(16),
           child: isAgentCardExpanded
               ? Column(
-                  children: [
-                    _buildHeader(),
-                    Expanded(child: _buildAgentCard()),
-                  ],
-                )
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildHeader(),
+              const SizedBox(height: 16),
+              Expanded(child: _buildAgentCard()),
+            ],
+          )
               : isRevenueCardExpanded
-                  ? Column(
-                      children: [
-                        _buildHeader(),
-                        Expanded(child: _buildRevenueCard()),
-                      ],
-                    )
-                  : SingleChildScrollView(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          _buildHeader(),
-                          _buildRevenueCard(),
-                          const SizedBox(height: 16),
-                          Row(
-                            children: [
-                              Expanded(child: _buildAgentCard()),
-                              const SizedBox(width: 12),
-                              const Expanded(
-                                child: SizedBox(
-                                  height: 120,
-                                  child: Placeholder(),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
+              ? Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildHeader(),
+              const SizedBox(height: 16),
+              Expanded(child: _buildRevenueCard()),
+            ],
+          )
+              : SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _buildHeader(),
+                const SizedBox(height: 24),
+                _buildRevenueCard(),
+                const SizedBox(height: 16),
+                Row(
+                  children: [
+                    Expanded(child: _buildAgentCard()),
+                    const SizedBox(width: 12),
+                    const Expanded(
+                      child: SizedBox(
+                        height: 120,
+                        child: Placeholder(),
                       ),
                     ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
